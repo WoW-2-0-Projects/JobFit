d9864411a0de567d847cd730ad9800117389c644
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-07-21 15:15:59 +0500

    Add EventBusBroker and IEventBusBroker class and interface mediatr package

---
+++ src/JobFit.Backend/JobFit.Application/Common/EventBus/Brokers/IEventBusBroker.cs
@@ -0,0 +1,8 @@
+﻿using JobFit.Domain.Common.Events;
+
+namespace JobFit.Application.Common.EventBus.Brokers;
+
+public interface IEventBusBroker
+{
+    ValueTask PublishLocalAsync<TEvent>(TEvent @event) where TEvent : EventBase;
+}
---
+++ src/JobFit.Backend/JobFit.Infrastructure/Common/Brokers/EventBusBroker.cs
@@ -0,0 +1,13 @@
+﻿using JobFit.Application.Common.EventBus.Brokers;
+using JobFit.Domain.Common.Events;
+using MediatR;
+
+namespace JobFit.Infrastructure.Common.Brokers;
+
+public class EventBusBroker(IPublisher mediator) : IEventBusBroker
+{
+    public ValueTask PublishLocalAsync<TEvent>(TEvent @event) where TEvent : EventBase
+    {
+        return new ValueTask(mediator.Publish(@event));
+    }
+}
