bdd50f3c1e25f49ca46b3d12e10e40622b9446ae
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-07-22 11:58:45 +0500

    Add employee and recruiter TPH relations

--- src/JobFit.Backend/JobFit.Persistence/DataContext/AppDbContext.cs
+++ src/JobFit.Backend/JobFit.Persistence/DataContext/AppDbContext.cs
@@ -6,7 +6,9 @@ namespace JobFit.Persistence.DataContext;
 public class AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : DbContext(dbContextOptions)
 {
     public DbSet<User> Users => Set<User>();
-    
+    public DbSet<Employee> Employees => Set<Employee>();
+
+    public DbSet<Recruiter> Recruiters => Set<Recruiter>();
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ src/JobFit.Backend/JobFit.Persistence/Migrations/20240722063922_Add_Employee_And_Recruiter_TPH_Relation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobFit.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Employee_And_Recruiter_TPH_Relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Discriminator",
+                table: "Users",
+                newName: "UserType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserType",
+                table: "Users",
+                newName: "Discriminator");
+        }
+    }
+}
