943818cc23ae6fb0df5cbc30f7e86e4e19bb019e
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-07-23 12:52:32 +0500

    Add storage files migration

---
+++ src/JobFit.Backend/JobFit.Persistence/Migrations/20240723075140_Add_StorageFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobFit.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_StorageFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:hstore", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "StorageFiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OriginalName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    PhysicalName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    ContentType = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Extension = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    IsTemporaryFile = table.Column<bool>(type: "boolean", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Size = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    PathVariables = table.Column<Dictionary<string, string>>(type: "hstore", nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ModifiedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StorageFiles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StorageFiles");
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:PostgresExtension:hstore", ",,");
+        }
+    }
+}
